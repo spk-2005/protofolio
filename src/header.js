5843344f537cf4decdf3ae76783bf09ce1e1be1e
spk-2005 <prasannasimha5002@gmail.com>
2025-05-13 18:19:07 +0530

    first commit

--- src/header.js
+++ src/header.js
@@ -1,11 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
+
 export default function Header() {
-  const [showmenu, setshowmenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [currentPath, setCurrentPath] = useState("/");
+  
+  // Set initial path on component mount
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
 
   const toggleMenu = () => {
-    setshowmenu((prev) => !prev);
+    setShowMenu((prev) => !prev);
   };
+
+  // Close menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (showMenu && !event.target.closest('nav') && !event.target.closest('#menuicon')) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showMenu]);
+
+  // Close menu when resizing to desktop
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 992 && showMenu) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [showMenu]);
 
   const navLinks = [
     { href: "/", label: "Home" },
@@ -18,42 +53,52 @@ export default function Header() {
   ];
 
   function CustomLink({ href, children }) {
-    const path = window.location.pathname;
     return (
-      <li className={path === href ? "active" : ""}>
-        <a href={href}>{children}</a>
+      <li className={currentPath === href ? "active" : ""}>
+        <a 
+          href={href}
+          onClick={() => {
+            setCurrentPath(href);
+            setShowMenu(false); // Close menu when clicking a link
+          }}
+        >
+          {children}
+        </a>
       </li>
     );
   }
 
   return (
-    <>
-      <section>
-        <div id="home-cont">
-          <div id="myname">
-            <h1>SIMHADRI PRASANNA KUMAR</h1>
-          </div>
-          <div id="menuicon" onClick={toggleMenu}>
-            {showmenu ? (
-              <span>&#x2716; </span>
-            ) : (
-              <span>&#9776; </span>
-            )}
-          </div>
-          <nav className={showmenu ? "show-menu" : ""}>
-            <ul>
-              {navLinks.map((link) => (
-                <CustomLink key={link.href} href={link.href}>
-                  {link.label}
-                </CustomLink>
-              ))}
-            </ul>
-            {showmenu ? (<><div id="icons">
-                <span id="atrate">@prasannakumar</span>
-                </div></>):(<></>)}
-          </nav>
+    <header className="header">
+      <div id="home-cont">
+        <div id="myname">
+          <h1>SIMHADRI PRASANNA KUMAR</h1>
         </div>
-      </section>
-    </>
+        
+        <div id="menuicon" onClick={toggleMenu}>
+          {showMenu ? (
+            <span className="close-icon">&#x2716;</span>
+          ) : (
+            <span className="menu-icon">&#9776;</span>
+          )}
+        </div>
+        
+        <nav className={showMenu ? "show-menu" : ""}>
+          <ul>
+            {navLinks.map((link) => (
+              <CustomLink key={link.href} href={link.href}>
+                {link.label}
+              </CustomLink>
+            ))}
+          </ul>
+          
+          {showMenu && (
+            <div id="icons">
+              <span id="atrate">@prasannakumar</span>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
   );
 }
--- src/skills.js
+++ src/skills.js
@@ -1,35 +1,116 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { FaPython, FaJava, FaReact, FaNodeJs, FaDatabase, FaAws } from 'react-icons/fa';
-import {  DiJavascript1, DiHtml5} from 'react-icons/di';
+import { 
+  FaPython, 
+  FaJava, 
+  FaReact, 
+  FaNodeJs, 
+  FaDatabase, 
+  FaAws, 
+  FaHtml5, 
+  FaCss3Alt, 
+  FaBootstrap, 
+  FaGitAlt, 
+  FaNpm, 
+  FaDocker,
+  FaServer,
+  FaCode,
+  FaBrain,
+  FaJs,
+  FaRProject,
+  FaCloud,
+  FaGoogle,
+  FaMicrosoft,
+  FaCuttlefish,
+  FaPlus
+} from 'react-icons/fa';
+import {
+  DiMongodb, 
+  DiMysql,
+  DiFirebase,
+  DiJqueryLogo,
+  DiSass,
+  DiCss3,
+  DiCode,
+  DiHaskell,
+  DiPostgresql,
+  DiPython,
+  DiReact
+} from 'react-icons/di';
+import { 
+  AiFillApi,
+  AiFillCode
+} from 'react-icons/ai';
+import { 
+  GrGraphQl
+} from 'react-icons/gr';
 import './skills.css';
 
-
 export default function Skills() {
-  const progskills = [
-    { name: 'C', icon:  <FaJava /> },
-    { name: 'C++', icon:  <FaJava /> },
-    { name: 'JAVA', icon: <FaJava /> },
+  // Programming Languages
+  const programmingLanguages = [
+    { name: 'C', icon: <FaCuttlefish /> },
+    { name: 'C++', icon: <><FaCuttlefish /><FaPlus /></> },
+    { name: 'Java', icon: <FaJava /> },
     { name: 'Python', icon: <FaPython /> },
-    { name: 'R', icon:  <FaJava /> },
-    { name: 'Machine Learning', icon: <FaDatabase /> }
+    { name: 'JavaScript', icon: <FaJs /> },
+    { name: 'TypeScript', icon: <AiFillCode /> },
+    { name: 'R', icon: <FaRProject /> },
+    { name: 'Haskell', icon: <DiHaskell /> }
   ];
 
-  const scrypting = [
-    { name: 'JavaScript', icon: <DiJavascript1 /> },
-    { name: 'React.Js', icon: <FaReact /> },
+  // Web Development
+  const webDevelopment = [
+    { name: 'HTML5', icon: <FaHtml5 /> },
+    { name: 'CSS3', icon: <FaCss3Alt /> },
+    { name: 'Sass', icon: <DiSass /> },
+    { name: 'jQuery', icon: <DiJqueryLogo /> },
+    { name: 'React.js', icon: <FaReact /> },
+    { name: 'Next.js', icon: <DiReact /> },
+    { name: 'Redux', icon: <AiFillCode /> },
+    { name: 'Material UI', icon: <FaCode /> },
+    { name: 'Tailwind CSS', icon: <DiCss3 /> },
+    { name: 'Bootstrap', icon: <FaBootstrap /> }
+  ];
+
+  // Backend Development
+  const backendDevelopment = [
     { name: 'Node.js', icon: <FaNodeJs /> },
-    { name: 'HTML & CSS', icon: <DiHtml5 /> },
-    { name: 'Express.Js', icon: <FaNodeJs /> }
+    { name: 'Express.js', icon: <AiFillApi /> },
+    { name: 'GraphQL', icon: <GrGraphQl /> },
+    { name: 'RESTful APIs', icon: <FaServer /> }
   ];
 
-  const DataBases = [
-    { name: 'MongoDB    ', icon: <FaDatabase/> },
-    { name: 'SQL', icon: <FaDatabase /> },
-    { name: 'Firebase', icon: <FaDatabase /> },
-    { name: 'Airtable', icon: <FaDatabase /> },
+  // AI & Machine Learning
+  const aiAndMachineLearning = [
+    { name: 'Machine Learning', icon: <FaBrain /> },
+    { name: 'Deep Learning', icon: <FaBrain /> },
+    { name: 'TensorFlow', icon: <DiPython /> },
+    { name: 'PyTorch', icon: <DiPython /> },
+    { name: 'Keras', icon: <DiPython /> },
+    { name: 'Natural Language Processing', icon: <FaCode /> },
+    { name: 'Computer Vision', icon: <DiCode /> }
+  ];
+
+  // Databases
+  const databases = [
+    { name: 'MongoDB', icon: <DiMongodb /> },
+    { name: 'SQL', icon: <DiMysql /> },
+    { name: 'PostgreSQL', icon: <DiPostgresql /> },
+    { name: 'Firebase', icon: <DiFirebase /> },
     { name: 'HBase', icon: <FaDatabase /> },
-    { name: 'AWS Console', icon: <FaAws /> }
+    { name: 'Airtable', icon: <FaDatabase /> }
+  ];
+
+  // Cloud & DevOps
+  const cloudAndDevOps = [
+    { name: 'AWS', icon: <FaAws /> },
+    { name: 'Google Cloud', icon: <FaGoogle /> },
+    { name: 'Azure', icon: <FaMicrosoft /> },
+    { name: 'Git', icon: <FaGitAlt /> },
+    { name: 'Docker', icon: <FaDocker /> },
+    { name: 'NPM', icon: <FaNpm /> },
+    { name: 'Hadoop', icon: <FaCloud /> }
   ];
 
   const cardAnimation = {
@@ -38,80 +119,44 @@ export default function Skills() {
     transition: { duration: 0.6, ease: 'easeOut' },
   };
 
+  const renderSkillCategory = (title, skills) => (
+    <>
+      <div className='skill-category'>
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: 'easeInOut' }}
+        >
+          {title}
+        </motion.h2>
+        <div className='card-container'>
+          {skills.map((skill, index) => (
+            <motion.div
+              key={index}
+              className='skill-card'
+              {...cardAnimation}
+              transition={{ duration: 0.8, delay: index * 0.1 }}
+            >
+              {skill.icon}
+              <span>{skill.name}</span>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+
   return (
     <div id='skills-section'>
-        <h1 id='skillhead'>Skills & Technologies I Can</h1>
+      <h1 id='skillhead'>Skills & Technologies</h1>
       
-      <div className='skill-category'>
-        <motion.h1
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: 'easeInOut' }}
-        >
-          Programming Skills
-        </motion.h1>
-        <div className='card-container'>
-          {progskills.map((skill, index) => (
-            <motion.div
-              key={index}
-              className='skill-card'
-              {...cardAnimation}
-              transition={{ duration: 0.8, delay: index * 0.1 }}
-            >
-              {skill.icon}
-              <span>{skill.name}</span>
-            </motion.div>
-          ))}
-        </div>
-      </div>
-      <hr/>
-      <div className='skill-category'>
-        <motion.h1
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: 'easeInOut' }}
-        >
-          Scripting Skills
-        </motion.h1>
-        <div className='card-container'>
-          {scrypting.map((skill, index) => (
-            <motion.div
-              key={index}
-              className='skill-card'
-              {...cardAnimation}
-              transition={{ duration: 0.8, delay: index * 0.1 }}
-            >
-              {skill.icon}
-              <span>{skill.name}</span>
-            </motion.div>
-          ))}
-        </div>
-      </div>
-
-      <hr/>
-      
-      <div className='skill-category'>
-        <motion.h1
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: 'easeInOut' }}
-        >
-          Databases
-        </motion.h1>
-        <div className='card-container'>
-          {DataBases.map((skill, index) => (
-            <motion.div
-              key={index}
-              className='skill-card'
-              {...cardAnimation}
-              transition={{ duration: 0.8, delay: index * 0.1 }}
-            >
-              {skill.icon}
-              <span>{skill.name}</span>
-            </motion.div>
-          ))}
-        </div>
-      </div>
+      {renderSkillCategory('Programming Languages', programmingLanguages)}
+      {renderSkillCategory('Web Development', webDevelopment)}
+      {renderSkillCategory('Backend Development', backendDevelopment)}
+      {renderSkillCategory('AI & Machine Learning', aiAndMachineLearning)}
+      {renderSkillCategory('Databases', databases)}
+      {renderSkillCategory('Cloud & DevOps', cloudAndDevOps)}
     </div>
   );
 }
